97c7a125fc9c70851095be92bf7b40b4bd2d0a8d
Gaberdios <gaberdios@gmail.com>
2016-08-28 19:51:19 +0200

    Design the UI till 07:38

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  PartyRockApp
+//
+//  Created by G on 28/08/16.
+//  Copyright © 2016 gaberdios. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+ 
+
+}
+
