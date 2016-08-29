d0fda2386454bcef876ad196c7e01266411c31be
Gaberdios <gaberdios@gmail.com>
2016-08-29 12:45:49 +0200

    final

--- PartyRockApp/PartyCell.swift
+++ PartyRockApp/PartyCell.swift
@@ -20,9 +20,22 @@ class PartyCell: UITableViewCell {
        
     }
 
-    func updateUI(PartyRock: PartyRock) {
-        videoTitle.text = PartyRock.videoTitle
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
         
+        let url = URL(string: partyRock.imageURL)!
+
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videopreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                // handle the error
+                
+            }
+        }
         
         
         
